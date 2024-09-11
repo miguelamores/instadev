740874119acf15739d8c35e012d132506f121315
Miguel Amores <migueamores@gmail.com>
2024-09-11 16:58:09 -0500

    create basic login

--- src/auth/SignIn.tsx
+++ src/auth/SignIn.tsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Link } from 'react-router-dom'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  username: z.string().min(2).max(10),
+  password: z.string().min(8)
+})
 
 const SignIn = () => {
-  return <div>SignIn</div>
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { username: '' }
+  })
+
+  const onSubmit = () => {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='w-full md:w-1/4 gap-4 flex flex-col'
+      >
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='johndoe' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder='*********' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Sign in</Button>
+      </form>
+      <p className='mt-4'>
+        Don't you have an account?{' '}
+        <Link to='/sign-up'>
+          {' '}
+          <strong>Register here</strong>
+        </Link>
+      </p>
+    </Form>
+  )
 }
 
 export default SignIn
