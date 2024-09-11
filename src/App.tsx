abe3b95c82e799b89c2996719da06de6dd01ab22
Miguel Amores <migueamores@gmail.com>
2024-09-11 15:25:42 -0500

    config auth layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import SignUp from './auth/SignUp'
 import SignIn from './auth/SignIn'
+import AuthLayout from './auth/AuthLayout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Routes>
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/sign-in' element={<SignIn />} />
+      <Route element={<AuthLayout />}>
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
+      </Route>
     </Routes>
   )
 }
