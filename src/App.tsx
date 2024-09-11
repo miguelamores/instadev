0d54f7b2dba9a665daa12049e6d897946b82093b
Miguel Amores <migueamores@gmail.com>
2024-09-11 15:22:51 -0500

    react router dom config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import SignUp from './auth/SignUp'
+import SignIn from './auth/SignIn'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-5xl text-red-800'>Insta for Devs</h1>
-    </>
+    <Routes>
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/sign-in' element={<SignIn />} />
+    </Routes>
   )
 }
 
