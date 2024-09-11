34e2680d8d1fc4c083a0e082b0391d858ef6b781
Miguel Amores <migueamores@gmail.com>
2024-09-11 16:54:11 -0500

    create basic sign in form

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen'>
       <h1>AuthLayout</h1>
       <Outlet />
     </div>
--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -2,24 +2,53 @@ import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Link } from 'react-router-dom'
 
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  name: z.string().min(2).max(10),
+  username: z.string().min(2).max(10),
+  email: z.string().email(),
+  password: z.string().min(8)
+})
 
 const SignUp = () => {
-  const form = useForm()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { username: '' }
+  })
 
   const onSubmit = () => {}
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='w-full md:w-1/4 gap-4 flex flex-col'
+      >
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder='John Doe' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name='username'
@@ -27,17 +56,50 @@ const SignUp = () => {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder='shadcn' {...field} />
+                <Input placeholder='johndoe' {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder='jdoe@gmail.com' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder='*********' {...field} />
+              </FormControl>
+
               <FormMessage />
             </FormItem>
           )}
         />
         <Button type='submit'>Submit</Button>
       </form>
+      <p className='mt-4'>
+        Do you already have an account?{' '}
+        <Link to='/sign-in'>
+          {' '}
+          <strong>Log in</strong>
+        </Link>
+      </p>
     </Form>
   )
 }
