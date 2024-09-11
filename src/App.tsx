9a35a2514bae848e2dea4f67abc2fb01554b7417
Miguel Amores <migueamores@gmail.com>
2024-09-11 11:14:00 -0500

    remove assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Insta for Devs</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
