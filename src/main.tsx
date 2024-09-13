79b7853c1ba32cb9b451f622ee3c575e174f268c
Miguel Amores <migueamores@gmail.com>
2024-09-13 12:15:15 -0500

    tanstack query config

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { Toaster } from './components/ui/toaster.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
