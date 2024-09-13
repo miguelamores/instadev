d7588e89c5e17a015a74de65c40484bdf758dd4d
Miguel Amores <migueamores@gmail.com>
2024-09-13 10:29:02 -0500

    move save to db to another fn

--- src/services/appwrite.ts
+++ src/services/appwrite.ts
@@ -15,17 +15,36 @@ export const createAccount = async (user: INewUser) => {
 
     const avatarUrl = avatar.getInitials(user.name) // TODO: verify if it's the correct method
 
+    const newUser = await saveUserToDB({
+      accountId: accountCreated.$id,
+      name: accountCreated.name,
+      email: accountCreated.email,
+      username: user.username,
+      imageUrl: avatarUrl
+    })
+
+    if (!newUser) throw Error
+
+    return newUser
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const saveUserToDB = async (user: {
+  accountId: string
+  email: string
+  name: string
+  imageUrl: URL
+  username?: string
+}) => {
+  try {
     const newUser = await database.createDocument(
       appwriteConfig.database,
       appwriteConfig.userCollection,
       ID.unique(),
-      {
-        name: accountCreated.name,
-        username: user.username,
-        accountId: accountCreated.$id,
-        email: accountCreated.email,
-        imageUrl: avatarUrl
-      }
+      user
     )
 
     if (!newUser) throw Error
