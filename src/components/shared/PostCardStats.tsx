8cd83891905292966dc403d1d3e2adea13c1435f
Miguel Amores <migueamores@gmail.com>
2024-11-06 18:16:24 -0500

    add hook and click event to save post

--- src/components/shared/PostCardStats.tsx
+++ src/components/shared/PostCardStats.tsx
@@ -1,3 +1,5 @@
+import { useGetCurrentUser } from '@/hooks/useGetCurrentUser'
+import useSavePosts from '@/hooks/useSavePosts'
 import { Models } from 'appwrite'
 
 type PostCardStatsType = {
@@ -6,6 +8,24 @@ type PostCardStatsType = {
 }
 
 const PostCardStats = ({ post, userId }: PostCardStatsType) => {
+  const { savePost, deleteSavedPost } = useSavePosts()
+  const { data: currentUser } = useGetCurrentUser()
+  console.log({ post })
+  console.log({ currentUser })
+
+  const savedPost = currentUser?.save.find(
+    (record: Models.Document) => record.post.$id === post.$id
+  )
+  console.log({ savedPost })
+
+  const handleSavePost = e => {
+    if (savedPost) {
+      deleteSavedPost.mutate(savedPost.$id)
+      return
+    }
+    savePost.mutate({ userId, postId: post.$id })
+  }
+
   return (
     <div className='flex items-center justify-between'>
       <div className='flex items-center'>
@@ -20,11 +40,16 @@ const PostCardStats = ({ post, userId }: PostCardStatsType) => {
       </div>
       <div className='flex items-center'>
         <img
-          src='/public/assets/icons/save.svg'
+          src={`${
+            savedPost
+              ? '/public/assets/icons/saved.svg'
+              : '/public/assets/icons/save.svg'
+          } `}
           alt='like'
-          className='w-6 h-6 stroke-white'
+          className='w-6 h-6 stroke-white cursor-pointer'
           width={24}
           height={24}
+          onClick={handleSavePost}
         />
       </div>
     </div>
