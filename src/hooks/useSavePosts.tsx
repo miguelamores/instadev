7d06fb98277eb58df9fc6f60253a551941e688a1
Miguel Amores <migueamores@gmail.com>
2024-11-06 20:58:55 -0500

    add optimistic update for saving post

--- src/hooks/useSavePosts.tsx
+++ src/hooks/useSavePosts.tsx
@@ -1,6 +1,7 @@
 import { QUERY_KEYS } from '@/consts'
 import { deleteSavedPost, savePost } from '@/services/appwrite'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Models } from 'appwrite'
 
 const useSavePosts = () => {
   const queryClient = useQueryClient()
@@ -8,11 +9,45 @@ const useSavePosts = () => {
   const savePostMutation = useMutation({
     mutationFn: ({ userId, postId }: { userId: string; postId: string }) =>
       savePost(userId, postId),
-    onSuccess: () => {
+    onMutate: async ({
+      userId,
+      postId
+    }: {
+      userId: string
+      postId: string
+    }) => {
+      await queryClient.cancelQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+      })
+
+      const previousUser = queryClient.getQueryData([
+        QUERY_KEYS.GET_CURRENT_USER
+      ])
+
+      queryClient.setQueryData(
+        [QUERY_KEYS.GET_CURRENT_USER],
+        (oldUser: Models.Document) => {
+          if (oldUser) {
+            return {
+              ...oldUser,
+              save: [...oldUser.save, { post: { $id: postId } }]
+            }
+          }
+        }
+      )
+
+      return { previousUser }
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(
+        [QUERY_KEYS.GET_CURRENT_USER],
+        context?.previousUser
+      )
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
       })
-
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_CURRENT_USER]
       })
@@ -21,7 +56,38 @@ const useSavePosts = () => {
 
   const unSavePostMutation = useMutation({
     mutationFn: (documentId: string) => deleteSavedPost(documentId),
-    onSuccess: () => {
+    onMutate: async (documentId: string) => {
+      await queryClient.cancelQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+      })
+
+      const previousUser = queryClient.getQueryData([
+        QUERY_KEYS.GET_CURRENT_USER
+      ])
+
+      queryClient.setQueryData(
+        [QUERY_KEYS.GET_CURRENT_USER],
+        (oldUser: Models.Document) => {
+          if (oldUser) {
+            return {
+              ...oldUser,
+              save: oldUser.save.filter(
+                (document: Models.Document) => document.$id !== documentId
+              )
+            }
+          }
+        }
+      )
+
+      return { previousUser }
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(
+        [QUERY_KEYS.GET_CURRENT_USER],
+        context?.previousUser
+      )
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
       })
