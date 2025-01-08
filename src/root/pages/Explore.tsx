18f4ad2e91fce9db264f21150ba56d96d9476773
Miguel Amores <migueamores@gmail.com>
2025-01-08 11:56:42 -0500

    create form to search posts

--- src/root/pages/Explore.tsx
+++ src/root/pages/Explore.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
 
 const Explore = () => {
-  return <div>Explore</div>
+  const form = useForm()
+
+  const onSubmit = (data: any) => {}
+
+  return (
+    <section>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='w-full md:w-2/4 gap-4 flex flex-col'
+        >
+          <FormField
+            control={form.control}
+            name='content'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Content</FormLabel>
+                <FormControl>
+                  <Input placeholder='What are you thinking?...' {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit'>Search</Button>
+        </form>
+      </Form>
+    </section>
+  )
 }
 
 export default Explore
