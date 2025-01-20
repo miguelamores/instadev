ba81ce61324f1f9aefdda9cb802892146d571dea
Miguel Amores <migueamores@gmail.com>
2025-01-20 11:52:42 -0500

    use shiny button

--- src/root/pages/Explore.tsx
+++ src/root/pages/Explore.tsx
@@ -1,5 +1,5 @@
 import { ExploreList } from '@/components/shared/ExploreList'
-import ShinyText from '@/components/shared/ShinyText'
+import { ShinyButton } from '@/components/shared/ShinyButton'
 import {
   Form,
   FormControl,
@@ -80,14 +80,7 @@ const Explore = () => {
         </h1>
       )}
       {isPending ? <p>Loading...</p> : <ExploreList posts={posts} />}
-      {hasNextPage && (
-        <button
-          className='text-gray-900 border w-full md:w-auto border-gray-300 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-700 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'
-          onClick={() => fetchNextPage()}
-        >
-          <ShinyText speed={3} text='Load more' />
-        </button>
-      )}
+      {hasNextPage && <ShinyButton text='Load more' onClick={fetchNextPage} />}
     </section>
   )
 }
