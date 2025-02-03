47786d8181d6f07cf62f73b0fec74c2cea123c54
Miguel Amores <migueamores@gmail.com>
2025-02-03 16:53:55 -0500

    cred hardcoded

--- e2e/setup/auth.setup.ts
+++ e2e/setup/auth.setup.ts
@@ -4,8 +4,8 @@ const authFile = 'e2e/.auth/user.json'
 
 setup('auth', async ({ page }) => {
   await page.goto('/sign-in')
-  await page.getByLabel('Email').fill(process.env.LOGIN_USERNAME!)
-  await page.getByLabel('Password').fill(process.env.LOGIN_PASS!)
+  await page.getByLabel('Email').fill('peter@gmail.com')
+  await page.getByLabel('Password').fill('1234567890')
   await page.getByRole('button', { name: 'Sign in' }).click()
 
   await page.waitForURL('/')
