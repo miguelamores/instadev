e1c26bd8b7731bd61f2db2c02a113eab38ccfb7b
Miguel Amores <migueamores@gmail.com>
2025-06-04 10:18:45 -0500

    refactor error handling function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,17 @@
 export function getErrorMessage(error: unknown) {
-  if (error instanceof Error) return error.message
-  return String(error)
+  let message: string
+
+  if (error instanceof Error) {
+    message = error.message
+  } else if (error && typeof error === 'object' && 'message' in error) {
+    // in this case, we convert the error to a string
+    // because it could be a number or any other type
+    message = String(error.message)
+  } else if (typeof error === 'string') {
+    message = error
+  } else {
+    message = 'Something went wrong, please try again later'
+  }
+
+  return message
 }
